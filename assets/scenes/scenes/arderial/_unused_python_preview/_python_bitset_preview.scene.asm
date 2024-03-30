a98e9bfa976fc4e3c6baf35edb0714fedc0240a7
GauChoob <starlight_tonight@hotmail.com>
2024-03-30 19:24:27 -0400

    Unused Naroom Forest maps

---
+++ assets/scenes/scenes/arderial/_unused_python_preview/_python_bitset_preview.scene.asm
@@ -0,0 +1,6 @@
+; Fake scene setup to allow python to generate a preview of the BITSET. Not used in the ROM file
+    AddressBank PAL_Arderial_Buildings_Outside
+    AddressBank BITSET_Arderial_Buildings_OutsideAlt_UNUSED
+    AddressBank PATTERN_Arderial_Buildings_OutsideRLE
+    AddressBank METAMAP_Arderial_Palace_Outside
+    AddressBank COLLMAP_Arderial_Palace_Outside
---
+++ assets/scenes/scenes/naroom/vash/top_orwinroom_observatory/_python_unusedpattern_preview.scene.asm
@@ -0,0 +1,6 @@
+; Fake scene setup to display the unused PATTERN_Naroom_Vash_Top_OrwinRoom_Observatory_UNUSED
+    AddressBank PAL_Naroom_Vash_Top_OrwinRoom_Observatory
+    AddressBank BITSET_Naroom_Vash_Top_OrwinRoom_Observatory
+    AddressBank PATTERN_Naroom_Vash_Top_OrwinRoom_Observatory_UNUSEDRLE ; We don't use PATTERN_Naroom_Vash_Top_OrwinRoom_Observatory_UNUSED since the observatory graphics was added directly into PATTERN_Naroom_IndoorsRLE
+    AddressBank METAMAP_Naroom_Vash_Top_OrwinRoom_Observatory
+    AddressBank COLLMAP_Naroom_Vash_Top_OrwinRoom_Observatory
