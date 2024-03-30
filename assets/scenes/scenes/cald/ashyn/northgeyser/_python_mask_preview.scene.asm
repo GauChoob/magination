019defd19f8540aa5ce87c158725dc278af18eaa
GauChoob <starlight_tonight@hotmail.com>
2024-03-30 10:28:45 -0400

    Adding Arderial Geyser Pipe Masks x 2, and Cald NorthGeyser Mask Adjusted preview script to preview masks as well - need to retroactively do it for the last 2 commits though

---
+++ assets/scenes/scenes/cald/ashyn/northgeyser/_python_mask_preview.scene.asm
@@ -0,0 +1,6 @@
+; Fake scene setup to allow python to generate a preview of the mask. Not used in the ROM file
+    AddressBank PAL_Cald_Ashyn_NorthGeyser
+    AddressBank BITSET_Cald_Ashyn_NorthGeyser
+    AddressBank PATTERN_Cald_Ashyn_NorthGeyserRLE
+    AddressBank METAMAP_Cald_Ashyn_NorthGeyser_Mask
+    AddressBank COLLMAP_Cald_Ashyn_NorthGeyser
