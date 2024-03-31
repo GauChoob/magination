97bdbd022b17bff3236ed0414d0ba5551755c27c
GauChoob <starlight_tonight@hotmail.com>
2024-03-30 22:37:13 -0400

    Misc_StartScreen_Main_Mask

---
+++ assets/scenes/scenes/misc/startscreen/main/_python_mask_preview.scene.asm
@@ -0,0 +1,6 @@
+; Fake scene setup to allow python to generate a preview of the mask. Not used in the ROM file
+    AddressBank PAL_Misc_StartScreen
+    AddressBank BITSET_Misc_StartScreen
+    AddressBank PATTERN_Misc_StartScreenRLE
+    AddressBank METAMAP_Misc_StartScreen_Main_Mask
+    AddressBank COLLMAP_Misc_StartScreen_Main_Mask
