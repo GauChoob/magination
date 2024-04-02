643d766e3f59c68e00ae799878fd00730e11a630
GauChoob <starlight_tonight@hotmail.com>
2024-04-02 17:11:21 -0400

    Arderial_Geyser_Pipes1_MiddlePipeSwitch_Mask

---
+++ assets/scenes/scenes/arderial/geyser/pipes1_middlepipeswitch/_python_mask_preview.scene.asm
@@ -0,0 +1,6 @@
+; Fake scene setup to allow python to generate a preview of the mask. Not used in the ROM file
+    AddressBank PAL_Arderial_Geyser
+    AddressBank BITSET_Arderial_Geyser
+    AddressBank PATTERN_Arderial_GeyserRLE
+    AddressBank METAMAP_Arderial_Geyser_Pipes1_MiddlePipeSwitch_Mask
+    AddressBank COLLMAP_Arderial_Geyser_Pipes1_MiddlePipeSwitch_Mask
