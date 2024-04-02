642589e0aed548b7814f85ebecaa98bb180522f6
GauChoob <starlight_tonight@hotmail.com>
2024-04-02 17:01:38 -0400

    Arderial_Geyser_Remix_Arderial_KeyMaze_Mask Arderial_Geyser_Remix_Naroom_East_Mask

---
+++ assets/scenes/scenes/arderial/geyser/remix_arderial_keymaze/_python_mask_preview.scene.asm
@@ -0,0 +1,6 @@
+; Fake scene setup to allow python to generate a preview of the mask. Not used in the ROM file
+    AddressBank PAL_Arderial_Geyser
+    AddressBank BITSET_Arderial_Geyser
+    AddressBank PATTERN_Arderial_GeyserRLE
+    AddressBank METAMAP_Arderial_Geyser_Remix_Arderial_KeyMaze_Mask
+    AddressBank COLLMAP_Arderial_Geyser_Remix_Arderial_KeyMaze_Mask
---
+++ assets/scenes/scenes/arderial/geyser/remix_naroom_east/_python_mask_preview.scene.asm
@@ -0,0 +1,6 @@
+; Fake scene setup to allow python to generate a preview of the mask. Not used in the ROM file
+    AddressBank PAL_Misc_Geyser
+    AddressBank BITSET_Misc_Geyser
+    AddressBank PATTERN_Misc_GeyserRLE
+    AddressBank METAMAP_Arderial_Geyser_Remix_Naroom_East_Mask
+    AddressBank COLLMAP_Arderial_Geyser_Remix_Naroom_East_Mask
