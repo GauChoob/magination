1bac3f526ebe747102bf01672a69a0290af9e3c4
GauChoob <starlight_tonight@hotmail.com>
2024-04-02 16:33:59 -0400

    Underneath_Mushroom_Room_UNUSED1/2

---
+++ assets/scenes/scenes/underneath/mushroom/room_unused1/_python_metamap_preview.scene.asm
@@ -0,0 +1,5 @@
+    AddressBank PAL_Misc_StoneWalls
+    AddressBank BITSET_Misc_StoneWalls
+    AddressBank PATTERN_Misc_StoneWallsRLE
+    AddressBank METAMAP_Underneath_Mushroom_Room_UNUSED1
+    AddressBank COLLMAP_Underneath_Mushroom_Room_UNUSED1_DUMMY
