cbca3d85a46577d88624a5e316f69a7ba20cc6ef
GauChoob <starlight_tonight@hotmail.com>
2024-04-02 18:20:28 -0400

    Misc_Teleport_UNUSED1,2,3,4

---
+++ assets/scenes/scenes/misc/teleport/unused1/_python_metamap_preview.scene.asm
@@ -0,0 +1,5 @@
+    AddressBank PAL_Misc_StoneWalls
+    AddressBank BITSET_Misc_StoneWalls
+    AddressBank PATTERN_Misc_StoneWallsRLE
+    AddressBank METAMAP_Misc_Teleport_UNUSED1
+    AddressBank COLLMAP_Misc_Teleport_UNUSED1
---
+++ assets/scenes/scenes/misc/teleport/unused2/_python_metamap_preview.scene.asm
@@ -0,0 +1,5 @@
+    AddressBank PAL_Misc_StoneWalls
+    AddressBank BITSET_Misc_StoneWalls
+    AddressBank PATTERN_Misc_StoneWallsRLE
+    AddressBank METAMAP_Misc_Teleport_UNUSED2
+    AddressBank COLLMAP_Misc_Teleport_UNUSED2_DUMMY
