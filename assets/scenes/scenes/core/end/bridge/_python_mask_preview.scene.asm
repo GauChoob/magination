a957e60557cb013dab5db077a4976c7625ff84d2
GauChoob <starlight_tonight@hotmail.com>
2024-04-03 09:55:50 -0400

    Core_End_Bridge_Mask

---
+++ assets/scenes/scenes/core/end/bridge/_python_mask_preview.scene.asm
@@ -0,0 +1,6 @@
+; Fake scene setup to allow python to generate a preview of the mask. Not used in the ROM file
+    AddressBank PAL_Core_End_Bridge
+    AddressBank BITSET_Core_End_Bridge
+    AddressBank PATTERN_Core_End_BridgeRLE
+    AddressBank METAMAP_Core_End_Bridge_Mask
+    AddressBank COLLMAP_Core_End_Bridge_Mask
