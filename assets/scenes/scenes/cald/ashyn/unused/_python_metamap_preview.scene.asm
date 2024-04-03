43468b15b0999e07c918b0869b0ace87954e7cff
GauChoob <starlight_tonight@hotmail.com>
2024-04-02 22:49:24 -0400

    Cald_Ashyn_UNUSED

---
+++ assets/scenes/scenes/cald/ashyn/unused/_python_metamap_preview.scene.asm
@@ -0,0 +1,7 @@
+; Fake scene setup to allow python to generate a preview of the metamap. Not used in the ROM file
+; A modified version of Cald_Ashyn_West_SmithRoom
+    AddressBank PAL_Cald_Indoors
+    AddressBank BITSET_Cald_Indoors
+    AddressBank PATTERN_Cald_IndoorsRLE
+    AddressBank METAMAP_Cald_Ashyn_UNUSED
+    AddressBank COLLMAP_Cald_Ashyn_UNUSED_DUMMY
--- assets/scenes/scenes/misc/teleport/unused1/_python_metamap_preview.scene.asm
+++ assets/scenes/scenes/misc/teleport/unused1/_python_metamap_preview.scene.asm
@@ -1,3 +1,4 @@
+; Fake scene setup to allow python to generate a preview of the metamap. Not used in the ROM file
     AddressBank PAL_Misc_StoneWalls
     AddressBank BITSET_Misc_StoneWalls
     AddressBank PATTERN_Misc_StoneWallsRLE
