ab7e0f43765453522468ce9e39db07f6ee168a98
GauChoob <starlight_tonight@hotmail.com>
2024-04-03 10:39:51 -0400

    Orothe_Geyser_Southwest_Mask

---
+++ assets/scenes/scenes/orothe/geyser/southwest/_python_mask_preview.scene.asm
@@ -0,0 +1,6 @@
+; Fake scene setup to allow python to generate a preview of the mask. Not used in the ROM file
+    AddressBank PAL_Orothe_Geyser_Indoors
+    AddressBank BITSET_Orothe_Geyser_Indoors
+    AddressBank PATTERN_Orothe_Geyser_IndoorsRLE
+    AddressBank METAMAP_Orothe_Geyser_Southwest_Mask
+    AddressBank COLLMAP_Orothe_Geyser_Southwest_Mask
