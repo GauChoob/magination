0de901e00552d8ed8217792d67089390ab83ba84
GauChoob <starlight_tonight@hotmail.com>
2024-04-03 09:47:20 -0400

    Cald_Caverns_PathwayU_CacheRoom_UNUSED1/2

---
+++ assets/scenes/scenes/cald/caverns/pathwayu_cacheroom_unused2/_python_metamap_preview.scene.asm
@@ -0,0 +1,6 @@
+; Fake scene setup to allow python to generate a preview of the metamap. Not used in the ROM file
+    AddressBank PAL_Misc_StoneWalls
+    AddressBank BITSET_Misc_StoneWalls
+    AddressBank PATTERN_Misc_StoneWallsRLE
+    AddressBank METAMAP_Cald_Caverns_PathwayU_CacheRoom_UNUSED2
+    AddressBank COLLMAP_Cald_Caverns_PathwayU_CacheRoom_UNUSED2
