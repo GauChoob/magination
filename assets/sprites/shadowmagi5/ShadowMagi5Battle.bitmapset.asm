b1fe3c16b1cc6918d80b27d2215b3575cb63f0e9
GauChoob <starlight_tonight@hotmail.com>
2024-04-11 17:54:42 -0400

    Sprites: CardsceneFX

--- assets/sprites/shadowmagi5/ShadowMagi5Battle.bitmapset.asm
+++ assets/sprites/shadowmagi5/ShadowMagi5Battle.bitmapset.asm
@@ -2,7 +2,7 @@
 
     ; Destination, Source, Width, Height
         LoadBitmap $8400, BITMAP_Battle_Summon, $10, $04
-        LoadBitmap $8800, BITMAP_Battle_Effects, $10, $07
+        LoadBitmap $8800, BITMAP_Battle_CardsceneFX, $10, $07
         LoadBitmap $8000, BITMAP_Battle_ShadowMagi5, $10, $02
 
     db 1         ; VRAM1 entries
