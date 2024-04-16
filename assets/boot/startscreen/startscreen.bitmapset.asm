8743fe60dbcd4aebd0e7a50422d43afda052a961
GauChoob <starlight_tonight@hotmail.com>
2024-04-16 15:17:05 -0400

    Rename boot assets to have Boot in their name

--- assets/boot/startscreen/startscreen.bitmapset.asm
+++ assets/boot/startscreen/startscreen.bitmapset.asm
@@ -1,10 +1,10 @@
     db $01         ; VRAM0 entries
 
     ; Destination, Source, Width, Height
-        LoadBitmap $8000,BITMAP_PressStart,$07,$01
+        LoadBitmap $8000,BITMAP_Boot_PressStart,$07,$01
 
     db $02         ; VRAM1 entries
 
     ; Destination, Source, Width, Height
-        LoadBitmap $9000,BITMAP_MagiNationLogo,$10,$07
+        LoadBitmap $9000,BITMAP_Boot_MagiNationLogo,$10,$07
         LoadBitmap $8EE0,BITMAP_Misc_BlankTiles,$02,$02
