1c01c0a2532ea26567b269a0644d021da34aa1f4
GauChoob <starlight_tonight@hotmail.com>
2024-04-16 16:27:44 -0400

    Bank 40 done

--- python/temp_assets.py
+++ python/temp_assets.py
@@ -12,8 +12,6 @@ sym = utils.SymFile('python/BITsym2.sym')
 sym_full = utils.SymFile()
 rom = utils.Rom()
 out = config.outdir
-sprite_pal = color.Palette.init_from_original_file('python/projutils/data/AllSprites.pal.png')
-sprite_stone_pal = color.Palette.init_from_original_file('python/PAL_Sprite_StoneMagi.pal.png')
 sprite_palettes = sprite.SpritePalettes()
 sprite_offsets = sprite.SpriteOffsets()
 
@@ -49,8 +47,6 @@ def colorize(ref: filereference.FileReference):
         offset += sprite_offsets.bases[offset_name]*0x10
     sprite_name = ref.label_name.rsplit('_', 1)[1]
     palette = color.Palette.init_from_original_file(sprite_palettes.get(sprite_name))
-    if ref.label_name.startswith('BITMAP_Sprite_Stone'):
-        palette = sprite_stone_pal.palette
     ref.contents.colorize_from_sprite(ref._spr_glob, vbk, offset, palette, 0, True, 0)
     return True
 
